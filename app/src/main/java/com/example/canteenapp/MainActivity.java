b589d0b5a2cb993c642d0565bbe44617df6c771e
sahil-777 <turbothunderboltz@gmail.com>
2021-10-16 22:05:59 +0530

    Reinitialize db connection

--- app/src/main/java/com/example/canteenapp/MainActivity.java
+++ app/src/main/java/com/example/canteenapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 // import com.google.firebase.database.DatabaseReference;
 // import com.google.firebase.database.FirebaseDatabase;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
