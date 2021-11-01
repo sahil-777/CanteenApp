89a9c45fcc26b229aaea7552936766b24fdf5438
sahil-777 <turbothunderboltz@gmail.com>
2021-11-02 02:17:39 +0530

    Login page added

---
+++ app/src/main/java/com/example/shoppingcart/views/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.shoppingcart.views;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shoppingcart.R;
+
+public class LoginActivity  extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
+
+
