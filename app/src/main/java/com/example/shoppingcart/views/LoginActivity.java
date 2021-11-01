e0afbf2ec40efb911f2d814a94bc9698d935eebd
sahil-777 <turbothunderboltz@gmail.com>
2021-11-02 03:49:28 +0530

    Updated login page

--- app/src/main/java/com/example/shoppingcart/views/LoginActivity.java
+++ app/src/main/java/com/example/shoppingcart/views/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.shoppingcart.views;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.shoppingcart.R;
 
@@ -9,6 +15,28 @@ public class LoginActivity  extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        TextView loginButton = (TextView) findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text1 = (EditText) findViewById(R.id.email_input);
+                String email = text1.getText().toString();
+
+                EditText text2 = (EditText) findViewById(R.id.password_input);
+                String password = text2.getText().toString();
+
+                // Toast.makeText(getBaseContext(), email + " " + password, Toast.LENGTH_LONG).show();
+                if (email.equals("user@canteenapp.com") && password.equals("ssssssss")) {
+                    Toast.makeText(getBaseContext(), "Logged in Successfully", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent( getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getBaseContext(), "Incorrect email or password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
 
